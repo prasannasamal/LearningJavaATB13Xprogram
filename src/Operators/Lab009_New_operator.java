febbd16d4f0d1c8b0593dc1c206f569ef403e008
prasannasamal <prasannasamal47@gmail.com>
2025-06-11 19:25:27 +0530

    new Operator updated

--- src/Operators/Lab009_New_operator.java
+++ src/Operators/Lab009_New_operator.java
@@ -3,7 +3,8 @@ package Operators;
 public class Lab009_New_operator {
     public static void main(String[] args) {
         //New operator--oops
-       String name = new String( original: "Pramod");
+       String s1 = new String( "Pramod" );
+        System.out.println(s1 instanceof String);
 
     }
 
--- src/Operators/Lab10_Instanceof_Operator.java
+++ src/Operators/Lab10_Instanceof_Operator.java
@@ -2,7 +2,7 @@ package Operators;
 
 public class Lab10_Instanceof_Operator {
     public static void main(String[] args) {
-        String s1 = new String(original: "Pramod");
+        String s1 = new String("Pramod");
         System.out.println(s1 instanceof String);
     }
 }
